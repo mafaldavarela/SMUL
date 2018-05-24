041326d8aa7a0e7ac9616c09d7794d020dc138ad
Francisco <franciscolfraga@gmail.com>
2018-05-24 14:54:17 +0100

    album added to index

--- js/clickable.js
+++ js/clickable.js
@@ -16,3 +16,18 @@ $(function() {
         $('#player_luz').removeClass("on");
     });
 });
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 600);
+        return false;
+      }
+    }
+  });
+});
